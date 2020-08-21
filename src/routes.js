5dee3dcd5b978624901341f5a21446edb04fa209
paulobeckman <beckmam.paulo@hotmail.com>
2020-08-21 20:49:56 -0300

    finish page home and working page events

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ const express = require('express')
 const routes = express.Router()
 
 const home = require('./app/controllers/home')
+const events = require('./app/controllers/events')
+
 
 routes.get("/", function(req, res){
     return res.redirect("/home")
 })
 
 routes.get("/home", home.index)
+
+routes.get("/events", events.index)
 
 module.exports = routes
